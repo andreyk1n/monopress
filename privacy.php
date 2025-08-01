c2141efbbb2ece7e14d201c9db10ec8bf78f453a
andreyk1n <betas23wr@gmail.com>
2025-08-01 21:07:49 +0300

    add privacy template and update styles

---
+++ privacy.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Template Name: Privacy Policy
+*/
+get_header();
+?>
+
+<section class="privacy">
+    <div class="privacy__container">
+        <h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+    </div>
+</section>
+
+<?php get_footer(); ?>
